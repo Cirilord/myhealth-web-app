272697c8697bce80982b7492048de9bca4f16240
Pedro Cirilo <phscirilo123@gmail.com>
2025-06-13 19:53:22 -0300

    chore: add rules for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,82 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            'jsx-quotes': [
+                'error',
+                'prefer-single'
+            ],
+            'quotes': [
+                'error',
+                'single'
+            ],
+            'semi': [
+                'error',
+                'always'
+            ]
+            // 'sort-keys': [
+            //     'error'
+            // ]
+        }
+    },
+    {
+        rules: {
+            'react/jsx-newline': [
+                'error',
+                {
+                    prevent: true
+                }
+            ],
+            'react/jsx-boolean-value': [
+                'error',
+                'always'
+            ],
+            'react/jsx-closing-bracket-location': [
+                'error',
+                'after-props'
+            ],
+            'react/jsx-equals-spacing': [
+                'error',
+                'never'
+            ],
+            'react/jsx-fragments': [
+                'error',
+                'element'
+            ],
+            'react/jsx-sort-props': [
+                'error',
+                {
+                    callbacksLast: false,
+                    ignoreCase: true,
+                    noSortAlphabetically: false,
+                    shorthandFirst: false,
+                    shorthandLast: false
+                }
+            ],
+            'react/jsx-tag-spacing': [
+                'error',
+                {
+                    afterOpening: 'never',
+                    beforeClosing: 'never',
+                    beforeSelfClosing: 'always',
+                    closingSlash: 'allow'
+                }
+            ],
+            'react/jsx-wrap-multilines': [
+                'error',
+                {
+                    arrow: 'parens',
+                    assignment: 'parens',
+                    condition: 'ignore',
+                    declaration: 'parens',
+                    logical: 'ignore',
+                    prop: 'ignore',
+                    return: 'parens'
+                }
+            ]
+        }
+    }
 ];
 
 export default eslintConfig;
